795cc7f4835ff3412db86baebe78249b0010a336
Alpistinho <danielmguimaraes@poli.ufrj.br>
2016-09-05 21:50:52 -0300

    Making it build and run

--- spiceLib/main.cpp
+++ spiceLib/main.cpp
@@ -8,14 +8,14 @@ int main(int argc, char *argv[])
     Spice::ComponentStorage componentStorage;
     Spice::FrequencySimulation freqSim;
     Spice::FileManager fileManager;
-    fileManager.readNetlist("circuit_description.txt",&componentStorage,&freqSim);
-    fileManager.writeResults(freqSim.simulateFrequencyResponse(&componentStorage));
+    // fileManager.readNetlist("circuit_description.txt",&componentStorage,&freqSim);
+    // fileManager.writeResults(freqSim.simulateFrequencyResponse(&componentStorage));
 
     Spice::ComponentStorage componentStorage2;
     Spice::TransientSimulation timeSim;
-    fileManager.readNetlist("circuit_description.txt", &componentStorage2, &timeSim);
+    // fileManager.readNetlist("circuit_description.txt", &componentStorage2, &timeSim);
     std::vector<double> initialConditions(componentStorage2.getSystemSize(), 0);
-    fileManager.writeResults(timeSim.simulateTransientResponse(&componentStorage2, initialConditions));
+    // fileManager.writeResults(timeSim.simulateTransientResponse(&componentStorage2, initialConditions));
 
     return 0;
 }
