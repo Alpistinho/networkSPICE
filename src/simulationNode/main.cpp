bbcf00e0d5d0834edb7124a6733c33569d6d44c5
Alpistinho <danielmguimaraes@poli.ufrj.br>
2016-11-15 11:02:17 -0200

    Autogenerating protobuf message sources

--- src/simulationNode/main.cpp
+++ src/simulationNode/main.cpp
@@ -2,11 +2,15 @@
 #include "FrequencySimulation.h"
 #include "TransientSimulation.h"
 
+#include "component.pb.h"
+
 int main(int argc, char *argv[]) {
     
     Spice::ComponentStorage componentStorage;
     Spice::FrequencySimulation freqSim;
     Spice::TransientSimulation timeSim;
+    
+    networkSpiceMessages::Component component;
 
     std::vector<double> initialConditions(componentStorage.getSystemSize(), 0);
 
