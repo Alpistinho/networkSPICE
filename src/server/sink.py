a98c8d84b905d398bbb0f8e04c3aff282672ae53
Gabriel Capella <gabrielcplla@poli.ufrj.br>
2016-12-04 20:34:02 -0200

    change the directory of sink.py

---
+++ src/server/sink.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import sys
+import math
+
+import zmq
+import time 
+
+import component_pb2
+import simulationrequest_pb2
+import results_pb2
+
+
+context = zmq.Context()
+
+# Socket to receive messages on
+receiver = context.socket(zmq.PULL)
+receiver.bind("tcp://*:5558")
+
+# array of frequency and transient points
+simReturn=results_pb2.Results()
+
+# Wait for start of batch
+s = receiver.recv()
+
+
+# Start our clock now
+tstart = time.time()
+
+simReturn.ParseFromString= receiver.recv()
+print(simReturn.__str__())
+
+print("Total elapsed time: %d msec" % ((tend-tstart)*1000))
+tend = time.time()
